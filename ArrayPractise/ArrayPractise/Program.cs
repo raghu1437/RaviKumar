15112ac711df9863c7c5fa5dd50235645278891c
GRavi-Kumar <97373126+GRavi-Kumar@users.noreply.github.com>
2022-01-27 18:55:15 +0530

    Add files via upload

---
+++ ArrayPractise/ArrayPractise/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+int[] Number=new int[5];
+Number[0]=10;
+Number[1]=11;
+Number[2]=12;   
+Number[3]=13;
+Number[4]=14;
+Console.WriteLine("There is an array of 5 Numbers");
+Console.WriteLine("Do you want to know the Array Element-Enter any number b/W 1 and 5");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("{0}", Number[i - 1]);
